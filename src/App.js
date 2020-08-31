a246cfd6b57b57b71184da1f750da14e1858a8d8
Amine Dammak <amine.dammak@habemus.com>
2020-08-31 16:27:03 +0100

    add bluetooth

--- src/App.js
+++ src/App.js
@@ -1,42 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { saveAs } from "file-saver";
 
-import { fetchWeather } from './api/fetchWeather';
-import './App.css';
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-    
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-            setWeather(data);
-            setQuery('');
-        }
+  const createfile = () => {
+    var file = new File(["foo"], "foo.txt", {
+      type: "text/plain",
+    });
+    saveAs(file, "filewithjs_pwa.txt");
+  };
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
     }
+  };
 
-    return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
+  const bluetooth = () => {
+    navigator.bluetooth
+      .requestDevice({
+        acceptAllDevices: true,
+      })
+      .then((device) => {
+        /* ... */
+        console.log("device", device.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="main-container">
+      <button onClick={bluetooth}>Connect via bluetooth</button>
+      <br />
+      <button onClick={createfile}>Generate file</button>
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default App;
